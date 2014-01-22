5fe058bda79ee0d76826b8fe8573c8a3a76eac32
Bryan Gilbert <gilbertw1@gmail.com>
2014-01-22 11:18:12 -0500

    Intial Commit of turbine data loader project

--- README.md
+++ README.md
@@ -1,4 +1,51 @@
 turbine-data-loader
 ===================
 
-A project to help load turbine data
+A turbine data loader written in clojure
+
+
+Getting Started
+---------------
+
+Compile
+
+    $ lein compile
+
+Run
+
+    $ lein run <conf.json> <optional input file>
+
+Package (Create Jar)
+
+    $ lein uberjar
+
+
+Running
+-------
+
+Provides two alternate modes
+
+Input file (Loads data via input file)
+
+    $ java -jar data-loader.jar <conf.json> <input.json>
+
+Stdin (Loads data via standard in
+
+    $ java -jar data-loader.jar <conf.json>)
+
+
+Configuration
+-------------
+
+```
+    {
+        "turbine-base": "http://localhost:8080",
+        "database": "mydb",
+        "collection": "mycoll"
+    }
+```
+
+Input
+-----
+
+All input (via file or stdin) is a list of json events delimited by a newline '\n'
---
+++ src/turbine_data_loader/core.clj
@@ -0,0 +1,43 @@
+(ns turbine-data-loader.core
+  (:gen-class)
+  (require [clojure.java.io :as io]
+           [clj-http.client :as client]
+           [cheshire.core :as json]))
+
+(def resource-conf (-> "config.json" io/resource))
+
+(defn read-conf [file]
+  (json/parse-string (slurp (or file resource-conf)) true))
+
+(defn post-event [turbine-url event-json]
+  (client/post turbine-url {:body event-json}))
+
+(defn make-turbine-url [{:keys [turbine-base database collection]}]
+  (str turbine-base "/db/" database "/" collection))
+
+(defn load-data-from-file [conf in-file]
+  (let [turbine-url (make-turbine-url conf)]
+    (with-open [rdr (io/reader (io/file in-file))]
+      (dorun 
+        (for [event-json (line-seq rdr)]
+          (post-event turbine-url event-json))))))
+
+(defn load-data-from-stdin [conf]
+  (let [turbine-url (make-turbine-url conf)]
+    (dorun 
+      (for [event-json (line-seq (java.io.BufferedReader. *in*))]
+        (post-event turbine-url event-json)))))
+
+(defn print-help-output []
+  (println
+    "USAGE:\n"
+    "    STDIN: java -jar data-loader.jar <conf.json>\n"
+    "    FILE:  java -jar data-loader.jar <conf.json> <input-file>\n"))
+
+(defn -main [& [conf-file in-file & args]]
+  (if-not conf-file
+    (print-help-output)
+    (let [conf (read-conf conf-file)]
+      (if in-file
+        (load-data-from-file conf in-file)
+        (load-data-from-stdin conf)))))
